57479a7d11aba4571a9b53acc2ae0d1af732e02f
Pedro Luiz <pedroluizlsg@gmail.com>
2023-04-12 18:26:40 -0300

    feat: complete_report implementado

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,17 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(data):
+        empresas = {}
+        for i in data:
+            if i['nome_da_empresa'] in empresas:
+                empresas[i['nome_da_empresa']] += 1
+            else:
+                empresas[i['nome_da_empresa']] = 1
+        continuacao_relatorio = '\nProdutos estocados por empresa:\n'
+        
+        for chave, valor in empresas.items():
+            continuacao_relatorio += f'- {chave}: {valor}\n'
+        
+        return SimpleReport.generate(data) + continuacao_relatorio
